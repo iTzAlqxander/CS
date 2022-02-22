46e938220c538e811f952549773097ffa7e53fce
Alexander Masztak <amasztak3281@stu.d214.org>
2022-02-22 14:56:22 -0600

    Created Project + Title Screen

--- CS/ViewController.swift
+++ CS/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var mapButton: UIButton!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapButton.layer.shadowColor = UIColor.black.cgColor
+            mapButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+            mapButton.layer.shadowRadius = 3
+        mapButton.layer.shadowOpacity = 1
+        
+        titleLabel.layer.shadowColor = UIColor.black.cgColor
+            titleLabel.layer.shadowOffset = CGSize(width: 3, height: 3)
+            titleLabel.layer.shadowRadius = 3
+            titleLabel.layer.shadowOpacity = 1.0
     }
 
-
+        
 }
 
