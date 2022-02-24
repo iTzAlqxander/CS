69309c0218373a45ead6e819e7876cdce9954bcb
Alex <amasztak3281@stu.d214.org>
2022-02-24 13:32:37 -0600

    New Loading Screen

--- CS/ViewController.swift
+++ CS/ViewController.swift
@@ -15,16 +15,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        //Shadow of map button
         mapButton.layer.shadowColor = UIColor.black.cgColor
-            mapButton.layer.shadowOffset = CGSize(width: 3, height: 3)
-            mapButton.layer.shadowRadius = 3
+        mapButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+        mapButton.layer.shadowRadius = 3
         mapButton.layer.shadowOpacity = 1
         
+        //Shadow of title
         titleLabel.layer.shadowColor = UIColor.black.cgColor
-            titleLabel.layer.shadowOffset = CGSize(width: 3, height: 3)
-            titleLabel.layer.shadowRadius = 3
-            titleLabel.layer.shadowOpacity = 1.0
-        //github test
+        titleLabel.layer.shadowOffset = CGSize(width: 1.5, height: 1.5)
+        titleLabel.layer.shadowRadius = 2
+        titleLabel.layer.shadowOpacity = 1.0
+        
+        
+        titleLabel.alpha = 1
+        mapButton.alpha = 1
     }
 
         
