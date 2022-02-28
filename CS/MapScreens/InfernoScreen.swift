ee74397a771cdcdb81eb44004e38693edcb27ba7
Alexander Masztak <amasztak3281@stu.d214.org>
2022-02-28 14:21:49 -0600

    Added to all maps

--- CS/MapScreens/InfernoScreen.swift
+++ CS/MapScreens/InfernoScreen.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class InfernoScreen: UIViewController {
 
+    @IBOutlet weak var tSwitch: UISwitch!
+    @IBOutlet weak var ctSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-
+    @IBAction func ctSideSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            tSwitch.setOn(false, animated: true)
+        } else {
+            tSwitch.setOn(true, animated: true)
+        }
+    }
+    @IBAction func tSideSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            ctSwitch.setOn(false, animated: true)
+        } else {
+            ctSwitch.setOn(true, animated: true)
+        }
+    }
 
 }
--- CS/MapScreens/TrainScreen.swift
+++ CS/MapScreens/TrainScreen.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class TrainScreen: UIViewController {
 
+    @IBOutlet weak var ctSwitch: UISwitch!
+    @IBOutlet weak var tSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    
+    @IBAction func ctSideSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            tSwitch.setOn(false, animated: true)
+        } else {
+            tSwitch.setOn(true, animated: true)
+        }
+    }
+    @IBAction func tSideSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            ctSwitch.setOn(false, animated: true)
+        } else {
+            ctSwitch.setOn(true, animated: true)
+        }
+    }
 
     
 }
