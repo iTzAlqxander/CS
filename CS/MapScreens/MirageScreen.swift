c2015c922a3d3c19f89fcdcf3a2b0806566cd5db
Alexander Masztak <amasztak3281@stu.d214.org>
2022-03-04 11:48:28 -0600

    Minimize and Maximize Button

--- CS/MapScreens/MirageScreen.swift
+++ CS/MapScreens/MirageScreen.swift
@@ -12,15 +12,59 @@ class MirageScreen: UIViewController {
     @IBOutlet weak var mirageMap: UIImageView!
     @IBOutlet weak var ctSwitch: UISwitch!
     @IBOutlet weak var tSwitch: UISwitch!
+    @IBOutlet weak var ctSideLabel: UILabel!
+    @IBOutlet weak var moltovLabel: UILabel!
+    @IBOutlet weak var tSideLabel: UILabel!
+    @IBOutlet weak var mimizeButton: UIButton!
+    @IBOutlet weak var smokeLabel: UILabel!
+    @IBOutlet weak var moltovSwitch: UISwitch!
+    @IBOutlet weak var buttonBack: UIImageView!
+    @IBOutlet weak var maximizeButton: UIButton!
+    @IBOutlet weak var smokeSwitch: UISwitch!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        /*
+        
+        
         mirageMap.isUserInteractionEnabled = true
         let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(self.pinchGesture))
         mirageMap.addGestureRecognizer(pinchGesture)
-        */
-       
+        
+    
+        maximizeButton.alpha = 0
+        
+    }
+
+    
+    @IBAction func whenMinPressed(_ sender: UIButton) {
+        ctSideLabel.alpha = 0
+        tSideLabel.alpha = 0
+        smokeLabel.alpha = 0
+        moltovLabel.alpha = 0
+        ctSwitch.alpha = 0
+        tSwitch.alpha = 0
+        smokeSwitch.alpha = 0
+        moltovSwitch.alpha = 0
+        buttonBack.alpha = 0
+        maximizeButton.alpha = 1
+        mimizeButton.alpha = 0
+    }
+    
+    @IBAction func whenMaxPressed(_ sender: UIButton) {
+        ctSideLabel.alpha = 1
+        tSideLabel.alpha = 1
+        smokeLabel.alpha = 1
+        moltovLabel.alpha = 1
+        ctSwitch.alpha = 1
+        tSwitch.alpha = 1
+        smokeSwitch.alpha = 1
+        moltovSwitch.alpha = 1
+        buttonBack.alpha = 1
+        maximizeButton.alpha = 0
+        mimizeButton.alpha = 1
     }
     
     @IBAction func ctSideSwitch(_ sender: UISwitch) {
@@ -37,12 +81,12 @@ class MirageScreen: UIViewController {
             ctSwitch.setOn(true, animated: true)
         }
     }
-    /*
+    
      @objc func pinchGesture(sender:UIPinchGestureRecognizer){
         sender.view?.transform = (sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)) as! CGAffineTransform
         sender.scale = 1.0
      
     }
-     */
+    
      
 }
